77c59040202ccfcc058a5fb4596d671dd55e418f
Salam Fazil <salaamfazil@icloud.com>
2023-04-16 19:12:30 -0700

    fixed few warnings

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import "./NavBar.css";
-import BcGovImg from "../../assets/images/bc-gov-logo-white.png"
 
 function NavBar() {
     return (
         <div className='nav-bar-stage'>
             <div className='nav-bar'>
                 <div className='info'>
-                    <img className="logo" src="https://developer.gov.bc.ca/static/BCID_H_rgb_rev-20eebe74aef7d92e02732a18b6aa6bbb.svg" />
+                    <img className="logo" src="https://developer.gov.bc.ca/static/BCID_H_rgb_rev-20eebe74aef7d92e02732a18b6aa6bbb.svg" alt="gov-logo" />
                     <h2 className='title'>AcronLog</h2>
                 </div>
             </div>
